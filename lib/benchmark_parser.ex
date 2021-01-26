34a4827bc91ad807ab283872eb3b2de3b1fcad66
Andre Orlov <to@andreorlov.ru>
2021-01-27 00:40:51 +0300

    Add flow case

--- lib/benchmark_parser.ex
+++ lib/benchmark_parser.ex
@@ -2,14 +2,21 @@ defmodule BenchmarkParser do
   @moduledoc false
 
   @words Helpers.words(9)
+  @qty_names 1000
+  @qty_descriptions 500
 
-  def words, do: @words
+  def benchmark do
+    Benchee.run(%{
+      "simple_enumerate" => & run/0,
+      "flow_enumerate"   => & run_flow/0
+    })
+  end
 
   def run do
-    names = Helpers.names(10)
+    names = Helpers.names(@qty_names)
     random_names = random_qty_words_in_names(names)
 
-    descriptions = random_insert_names(random_names, 20) |> IO.inspect(label: ANDRE_DESCRIPTIONS)
+    descriptions = random_insert_names(random_names, @qty_descriptions)
 
     Enum.map(descriptions, fn description ->
       rank =
@@ -21,11 +28,33 @@ defmodule BenchmarkParser do
     end)
   end
 
+  def run_flow do
+    names = Helpers.names(@qty_names)
+    random_names = random_qty_words_in_names(names)
+
+    descriptions = random_insert_names(random_names, @qty_descriptions)
+
+    descriptions
+    |> Flow.from_enumerable()
+    |> Flow.partition()
+    |> Flow.map(fn description ->
+      rank =
+        names
+        |> Flow.from_enumerable()
+        |> Flow.partition()
+        |> Flow.map(fn name ->
+          find_name(description, name)
+        end)
+        |> Enum.to_list()
+
+      {description, rank}
+    end)
+    |> Enum.to_list()
+  end
+
   # private
 
-
   defp find_name(str, [f | iii] = name) do
-    IO.inspect({{str, name}}, label: ANDRE_FIND_NAME)
     words = String.split(str, ~r/\s+/)
     case Enum.split_while(words, fn x -> x != f end) do
       {_, [_ | names]} ->
@@ -48,16 +77,12 @@ defmodule BenchmarkParser do
     1..qty
     |> Enum.map(fn _ ->
       @words
-      |> IO.inspect(label: ANDRE_WORDS)
       |> Enum.split(2)
       |> Tuple.to_list()
       |> Enum.concat([Enum.random(random_names)])
-      |> IO.inspect(label: ANDRE_WORDS_2)
       |> Enum.shuffle()
-      |> IO.inspect(label: ANDRE_WORDS_3)
       |> List.flatten()
       |> Enum.join(" ")
-      |> IO.inspect(label: ANDRE_WORDS_4)
     end)
   end
 
@@ -72,41 +97,5 @@ defmodule BenchmarkParser do
       |> Enum.take(qty)
       |> Enum.shuffle()
     end)
-  end
-
-  # def deep_join([], str) do
-  #   IO.inspect(label: ANDRE_D0)
-  #  []
-  # end
-  def deep_join([head | tail], str) when is_list(head) do
-    IO.inspect({head, tail}, label: ANDRE_D1)
-    res = deep_join(head, str)
-    res |> IO.inspect(label: ADDRE_D1_1)
-    [res | deep_join(tail, str)]
-  end
-  def deep_join([head | tail] = list, str) when is_list(tail) do
-    IO.inspect({head, tail}, label: ANDRE_D2)
-    [Enum.join(list, str), deep_join(tail, str)]
-  end
-  def deep_join(list, str) do
-    IO.inspect({list}, label: ANDRE_D3)
-    [Enum.join(list, str)]
-  end
-
-  def flatten([]), do: []
-  def flatten([head | tail]) when is_list(head) do
-	  [flatten(flatten(head) ++ tail) |> Enum.join(" ")]
-  end
-  def flatten([head | tail]), do: [head | flatten(tail)]
-
-  def t do
-    [
-      ["aaaa", "bbbb"],
-      [
-        ["FL", "AS"],
-        ["QLMHAWTCI", "AOPDP"],
-        ["SDBVWZ", "EADFROQ"]
-      ]
-  ]
   end
 end
